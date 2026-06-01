9773cb0b032d67182f7cf81404a32f5f478774eb
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-06-01 16:50:02 -0300

    feat: adiciona anotações de validação e geração de ID à classe User

--- userService/demo/src/main/java/com/ecommerce/demo/Model/User.java
+++ userService/demo/src/main/java/com/ecommerce/demo/Model/User.java
@@ -1,12 +1,25 @@
 package com.ecommerce.demo.Model;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+
 public class User {
 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
     protected String name;
+
+    @Email
     protected String email;
+
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     protected String password;
     
+    @Enumerated(EnumType.STRING)
     protected UserStatus status;
     
     public User(String name, String email, String password, UserStatus status) {
