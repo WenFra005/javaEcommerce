09cd6a7d1799948391c5b90be06806f3a76d1fa2
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-06-01 16:42:35 -0300

    feat: adiciona classe User com atributos e métodos para gerenciamento de usuários

---
+++ userService/demo/src/main/java/com/ecommerce/demo/Model/User.java
@@ -0,0 +1,51 @@
+package com.ecommerce.demo.Model;
+
+public class User {
+
+    protected String name;
+    protected String email;
+    protected String password;
+    protected UserStatus status;
+    
+    public User(String name, String email, String password, UserStatus status) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(UserStatus status) {
+        this.status = status;
+    }
+
+    
+
+}
