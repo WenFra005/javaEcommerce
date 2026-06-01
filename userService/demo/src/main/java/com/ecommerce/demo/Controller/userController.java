21e8224893349daa9caf05194bdc52dc27e6e753
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-06-01 17:56:27 -0300

    feat: cria controlador de usuários com métodos para criação e busca de usuário

---
+++ userService/demo/src/main/java/com/ecommerce/demo/Controller/userController.java
@@ -0,0 +1,34 @@
+package com.ecommerce.demo.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.demo.Model.User;
+import com.ecommerce.demo.Service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+@RestController
+@RequestMapping("/users")
+public class userController {
+
+    private final UserService userService = new UserService();
+
+    @PostMapping
+    public User postUser(@RequestBody User user) {
+        //TODO: process POST request
+
+        
+        return userService.createUser(user);
+    }
+    
+    @GetMapping("/{id}")
+    public User getMethodName(@PathVariable Long id) {
+        return userService.findUserById(id);
+    }
+    
+}
