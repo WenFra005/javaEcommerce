545a86e98c544f8bee0ca390661501567bcc820e
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-06-01 17:49:49 -0300

    feat: adiciona anotações @Entity e @Id à classe User

--- userService/demo/src/main/java/com/ecommerce/demo/Model/User.java
+++ userService/demo/src/main/java/com/ecommerce/demo/Model/User.java
@@ -4,15 +4,19 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 
+@Entity
 public class User {
 
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long userId;
     protected String name;
