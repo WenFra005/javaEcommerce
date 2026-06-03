fedae808c76347ca537b7655c71f735c26349323
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-06-03 14:01:21 -0300

    refactor: renomeia a classe CreateUserRequest para solicitação de criação de usuários

--- userService/demo/src/main/java/com/ecommerce/demo/dto/CreateUserRequest.java
+++ userService/demo/src/main/java/com/ecommerce/demo/dto/CreateUserRequest.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class createUserRequest {
+public class CreateUserRequest {
 
     @NotBlank(message = "Name is required")
     private String name;
