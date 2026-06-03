c48952ae03ebb131efbae0b8c734648e2e2ab5a4
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-06-03 13:32:34 -0300

    feat: adiciona a classe ErrorResponse para tratamento de erros

---
+++ userService/demo/src/main/java/com/ecommerce/demo/dto/ErrorResponse.java
@@ -0,0 +1,65 @@
+package com.ecommerce.demo.dto;
+
+import java.time.LocalDateTime;
+
+public class ErrorResponse {
+
+    private int code;
+    private String message;
+    private String error;
+    private LocalDateTime timestamp;
+    private String path;
+    
+    public ErrorResponse() {
+    }
+
+    public ErrorResponse(int code, String message, String error, LocalDateTime timestamp, String path) {
+        this.code = code;
+        this.message = message;
+        this.error = error;
+        this.timestamp = timestamp;
+        this.path = path;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    
+}
