954058098d4bf44c22dfcca755b52408055af6d8
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-06-03 12:50:39 -0300

    feat: cria a classe createUserRequest com validações para criação de usuário

---
+++ userService/demo/src/main/java/com/ecommerce/demo/dto/createUserRequest.java
@@ -0,0 +1,28 @@
+package com.ecommerce.demo.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class createUserRequest {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
+    private String userEmail;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String userPassword;
+
+    
+
+}
