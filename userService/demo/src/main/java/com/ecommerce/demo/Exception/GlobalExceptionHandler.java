26d3db45373694166cc362d71c212045ae425629
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-06-03 13:33:52 -0300

    feat: adiciona tratamento de exceção para usuário não encontrado na classe GlobalExceptionHandler

--- userService/demo/src/main/java/com/ecommerce/demo/Exception/GlobalExceptionHandler.java
+++ userService/demo/src/main/java/com/ecommerce/demo/Exception/GlobalExceptionHandler.java
@@ -1,5 +1,30 @@
 package com.ecommerce.demo.Exception;
 
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import com.ecommerce.demo.dto.ErrorResponse;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException exception, WebRequest request) {
+        
+        ErrorResponse errorResponse = new ErrorResponse(
+            HttpStatus.NOT_FOUND.value(),
+            exception.getMessage(), 
+            "User not found", 
+            LocalDateTime.now(),
+             request.getDescription(false).replace("uri=", "")
+            );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+    
 }
