16ffc452dd73ae362db5c80bb3abfb433fb5708b
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-06-17 13:30:40 -0300

    refactor(UserRepository): remover anotação @Repository
    Quando uma interface estende JpaRepository, a anotação @Repository é opcional e o Spring gerencia o bean automaticamente.
    Removendo a anotação evita redundância e mantém a definição mais enxuta.

--- userService/ecommerce/src/main/java/com/ecommerce/userservice/Repository/UserRepository.java
+++ userService/ecommerce/src/main/java/com/ecommerce/userservice/Repository/UserRepository.java
@@ -1,11 +1,8 @@
 package com.ecommerce.userservice.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.ecommerce.userservice.Model.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
