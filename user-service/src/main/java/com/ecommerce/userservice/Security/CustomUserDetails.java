6495dde972da94f8ff9eafb993114506c680006c
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-06-17 17:39:05 -0300

    feat(Security): criar CustomUserDetails
    - Implementa UserDetails usando a entidade User para integrar com Spring Security.
    - Permite a autenticação baseada em email/senha, definindo permissões vazias por padrão e expondo o objeto User quando necessário.

---
+++ user-service/src/main/java/com/ecommerce/userservice/Security/CustomUserDetails.java
@@ -0,0 +1,61 @@
+package com.ecommerce.userservice.Security;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.ecommerce.userservice.Model.User;
+
+public class CustomUserDetails implements UserDetails{
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO Auto-generated method stub
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getUserPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    
+    @Override
+    public boolean isEnabled() {
+        return true;   
+
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+}
