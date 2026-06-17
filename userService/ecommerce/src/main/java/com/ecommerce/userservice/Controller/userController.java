22e6c0965bb81ee3808d927693faac60cfbf632b
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-06-17 13:20:52 -0300

    refactor: aprimora validação e response handling do UserController

--- userService/ecommerce/src/main/java/com/ecommerce/userservice/Controller/userController.java
+++ userService/ecommerce/src/main/java/com/ecommerce/userservice/Controller/userController.java
@@ -7,8 +7,11 @@ import com.ecommerce.userservice.Service.UserService;
 import com.ecommerce.userservice.dto.CreateUserRequest;
 import com.ecommerce.userservice.dto.UserResponse;
 
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,37 +31,25 @@ public class userController {
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUser(@PathVariable Long id) {
-        UserResponse userResponse = userService.findUserById(id);
-
-        return ResponseEntity.ok(userResponse);
+        return ResponseEntity.ok(userService.findUserById(id));
     }
 
     @PostMapping("/create")
-    public ResponseEntity<UserResponse> postUser(@RequestBody CreateUserRequest request) {
-        // TODO: process POST request
+    public ResponseEntity<UserResponse> postUser(@Valid @RequestBody CreateUserRequest request) {
         UserResponse userResponse = userService.createUser(request);
 
-        return ResponseEntity.status(201).body(userResponse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userResponse);
     }
 
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<UserResponse> putUser(@RequestBody CreateUserRequest entity, @PathVariable Long id) {
-        //TODO: process PUT request
-        UserResponse userResponse = userService.updateUser(id, entity);
-        if (userResponse == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(userResponse);
+    public ResponseEntity<UserResponse> putUser(@Valid @RequestBody CreateUserRequest request, @PathVariable Long id) {
+
+        return ResponseEntity.ok(userService.updateUser(id, request));
     }
 
     @DeleteMapping("/delete/{id}") 
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
-        UserResponse user = userService.findUserById(id);
-        if (user == null) {
-            return ResponseEntity.notFound().build();
-        }
-
         userService.deleteUser(id);
 
         return ResponseEntity.noContent().build();
