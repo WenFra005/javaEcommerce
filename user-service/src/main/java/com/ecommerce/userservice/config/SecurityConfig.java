50623e8ddba57da55463fd217333a512736975ba
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-06-18 12:09:32 -0300

    feat(security): configurar segurança JWT no application
    Implementa a classe SecurityConfig que define:
    
    - PasswordEncoder com BCrypt.
    - AuthenticationManager via AuthenticationConfiguration.
    - SecurityFilterChain que desativa CSRF, permite /auth/login e /users/create, exige autenticação em outras rotas, injeta JwtAuthFilter antes do UsernamePasswordAuthenticationFilter e aplica política de sessão stateless.
    Essas alterações estabelecem a base de autenticação baseada em JWT e garantem que apenas endpoints específicos sejam acessíveis sem token.

---
+++ user-service/src/main/java/com/ecommerce/userservice/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.ecommerce.userservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.ecommerce.userservice.Security.JwtAuthFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception{
+        return authConfig.getAuthenticationManager();
+    }
+    
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/login", "/users/create").permitAll()
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
