16553737935b00cbd422b081781169c260c26022
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-06-19 14:35:40 -0300

    feat(security): criar ponto de entrada de autenticação personalizado
    Implementa CustomAuthenticationEntryPoint que:
    
    - Intercepta exceções de autenticação.
    Retorna resposta JSON estruturada com ErrorResponse.
    - Configura status 401 e cabeçalho application/json.
    Esta classe facilita a centralização da resposta de erro de acesso não autorizado.

---
+++ user-service/src/main/java/com/ecommerce/userservice/Security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,41 @@
+package com.ecommerce.userservice.Security;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import com.ecommerce.userservice.dto.ErrorResponse;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import tools.jackson.databind.ObjectMapper;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint{
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+        AuthenticationException authException) throws IOException, ServletException {
+        
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.UNAUTHORIZED.value(),
+                authException.getMessage(),
+                "Unauthorized",
+                LocalDateTime.now(),
+                request.getRequestURI()
+            );
+            response.getWriter().write(new ObjectMapper().writeValueAsString(errorResponse));
+    
+    }
+
+
+
+
+}
