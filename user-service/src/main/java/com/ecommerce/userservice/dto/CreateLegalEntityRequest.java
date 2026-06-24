2cd2d93bf367d8fab710ddca7bdab8a0ff02be35
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-06-24 16:10:16 -0300

    feat(dto): implementar CreateLegalEntityRequest com campos de CNPJ, nome da empresa e inscrição estadual
    A classe `CreateLegalEntityRequest` foi criada estendendo `CreateUserRequest` e adiciona:
    - Campo `cnpj` com validação de obrigatoriedade.
    - Campo `companyName` para nome da empresa, obrigatório.
    - Campo `stateRegistration` para inscrição estadual, obrigatório.
    - Construtores, getters e setters correspondentes.
    
    Essas propriedades permitem receber e validar as informações específicas de pessoa jurídica ao criar usuários.

---
+++ user-service/src/main/java/com/ecommerce/userservice/dto/CreateLegalEntityRequest.java
@@ -0,0 +1,54 @@
+package com.ecommerce.userservice.dto;
+
+import com.ecommerce.userservice.Enums.UserRole;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class CreateLegalEntityRequest extends CreateUserRequest {
+
+    @NotBlank(message = "CNPJ is required")
+    private String cnpj;
+
+    @NotBlank(message = "Company name is required")
+    private String companyName;
+
+    @NotBlank(message = "State registration is required")
+    private String stateRegistration;
+
+    public CreateLegalEntityRequest() {
+        super();
+    }
+
+    public CreateLegalEntityRequest(String name, String userEmail, String userPassword, UserRole userRole, String cnpj, String companyName, String stateRegistration) {
+        super(name, userEmail, userPassword, userRole);
+        this.cnpj = cnpj;
+        this.companyName = companyName;
+        this.stateRegistration = stateRegistration;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getStateRegistration() {
+        return stateRegistration;
+    }
+
+    public void setStateRegistration(String stateRegistration) {
+        this.stateRegistration = stateRegistration;
+    }
+
+
+}
