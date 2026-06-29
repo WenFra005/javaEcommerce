162c5ffc72087aaf06bb2253481409f4a3df81d5
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-06-29 11:50:48 -0300

    feat(security): habilitar rotas de cadastro e exigir autenticação para usuários
    - Atualizadas as permissões no `SecurityConfig`:
      - Incluídas rotas de registro de pessoa física (`/natural-person/register`) e jurídica (`/legal-entities/register`) como públicas.
      - Definida obrigatoriedade de autenticação para todos os endpoints sob `/users/**`.
    - Mantidos permissão para login e documentação Swagger, reforçando a segurança do serviço.

--- user-service/src/main/java/com/ecommerce/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/com/ecommerce/userservice/config/SecurityConfig.java
@@ -42,7 +42,8 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/users/create","/auth/login","/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                .requestMatchers("/natural-person/register", "/legal-entities/register","/auth/login","/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                .requestMatchers("users/**").authenticated()
                 .anyRequest().authenticated()
             )
             .exceptionHandling(exception -> exception
