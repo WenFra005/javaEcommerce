0b27ba4d70138b95b5804879c377e6bf6851d503
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-06-30 12:27:53 -0300

    feat(controller): adicionar anotações OpenAPI ao NaturalPersonController
    - Incluídas anotações `@Tag`, `@Operation` e `@ApiResponses` para documentação Swagger.
    - Mapeados códigos de resposta: 201 (usuário criado), 400 (requisição inválida) e 409 (conflito de e-mail).
    - Vinculados schemas `UserResponse` e `ErrorResponse` às respostas da API.

--- user-service/src/main/java/com/ecommerce/userservice/Controller/NaturalPersonController.java
+++ user-service/src/main/java/com/ecommerce/userservice/Controller/NaturalPersonController.java
@@ -5,8 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.userservice.Service.UserService;
 import com.ecommerce.userservice.dto.CreateNaturalPersonRequest;
+import com.ecommerce.userservice.dto.ErrorResponse;
 import com.ecommerce.userservice.dto.UserResponse;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 import org.springframework.http.HttpStatus;
@@ -17,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/natural-persons")
+@Tag(name = "Natural Person Controller", description = "Endpoints para gerenciar usuários do tipo Pessoa Física ou Natural")
 public class NaturalPersonController {
 
     private final UserService userService;
@@ -24,7 +32,13 @@ public class NaturalPersonController {
     public NaturalPersonController(UserService userService) {
         this.userService = userService;
     }
-
+    
+    @Operation(summary = "Registrar Pessoa Física ou Natural", description = "Cria um novo usuário do tipo Pessoa Física ou Natural no sistema.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Usuário criado com sucesso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponse.class))),
+        @ApiResponse(responseCode = "400", description = "Requisição inválida, dados do usuário não atendem aos critérios de validação", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+        @ApiResponse(responseCode = "409", description = "Conflito, o e-mail fornecido já está em uso", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @PostMapping("/register")
     public ResponseEntity<UserResponse> postRegister(@RequestBody @Valid CreateNaturalPersonRequest request) {
         UserResponse response = userService.createNaturalPerson(request);
