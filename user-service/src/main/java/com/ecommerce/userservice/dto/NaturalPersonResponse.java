fa5f16896c9ab9384bb37605b48d7366d11ad17e
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-07-01 20:55:21 -0300

    style(dto): padronizar ordem das propriedades no JSON de NaturalPersonResponse
    - Adicionada anotação `@JsonPropertyOrder` para definir ordem explícita das propriedades no JSON.
    - Removido espaço em branco final para padronização do estilo de código.

--- user-service/src/main/java/com/ecommerce/userservice/dto/NaturalPersonResponse.java
+++ user-service/src/main/java/com/ecommerce/userservice/dto/NaturalPersonResponse.java
@@ -2,10 +2,14 @@ package com.ecommerce.userservice.dto;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 
+@JsonPropertyOrder({ "userId", "userName", "userEmail", "userStatus", "userRole", "userType", "userCreatedAt", "cpf",
+        "birthDate" })
 @Schema(description = "Representa a resposta de um usuário do tipo Pessoa Física ou Natural Person, incluindo informações específicas desse tipo de usuário.")
-public class NaturalPersonResponse extends UserResponse{
+public class NaturalPersonResponse extends UserResponse {
 
     private String cpf;
 
@@ -35,5 +39,4 @@ public class NaturalPersonResponse extends UserResponse{
         this.birthDate = birthDate;
     }
 
-    
 }
