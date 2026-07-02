9ead2fdd6b6e1713cdae11d81dbdc068f07c693a
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-07-01 21:21:54 -0300

    refactor(user-service): remover método de tratamento de exceção da classe de domínio
    O método `handleUserNotFoundException` foi eliminado da classe `UserNotFoundException`. O tratamento da exceção já é realizado por um controlador de exceções global, tornando o método interno redundante. Sua remoção simplifica a classe, reduz o acoplamento com a camada de apresentação e evita duplicação de lógica.

--- user-service/src/main/java/com/ecommerce/userservice/Exception/UserNotFoundException.java
+++ user-service/src/main/java/com/ecommerce/userservice/Exception/UserNotFoundException.java
@@ -1,33 +1,9 @@
 package com.ecommerce.userservice.Exception;
 
-import java.time.LocalDateTime;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import com.ecommerce.userservice.dto.ErrorResponse;
-
-import jakarta.servlet.http.HttpServletRequest;
-
-public class UserNotFoundException extends RuntimeException{
+public class UserNotFoundException extends RuntimeException {
 
     public UserNotFoundException(String message) {
         super(message);
     }
-    
-    @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleUserNotFoundException(HttpServletRequest request) {
-        
-        ErrorResponse errorResponse = new ErrorResponse(
-            HttpStatus.NOT_FOUND.value(),
-            getMessage(), 
-            "User not found", 
-            LocalDateTime.now(),
-             request.getRequestURI()
-            );
-    
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-    }
-    
+
 }
