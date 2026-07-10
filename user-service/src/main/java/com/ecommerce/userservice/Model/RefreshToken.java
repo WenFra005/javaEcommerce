36b7cb19045da49f4771eff6167aca1a69aaa6ec
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-07-10 11:56:46 -0300

    feat(refresh-token): cria modelo de refresh token

---
+++ user-service/src/main/java/com/ecommerce/userservice/Model/RefreshToken.java
@@ -0,0 +1,81 @@
+package com.ecommerce.userservice.Model;
+
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity(name = "refresh_tokens")
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private Instant expiryDate;
+
+    @Column(nullable = false)
+    private boolean revoked;
+
+    @Column(nullable = false)
+    private User user;
+
+    public RefreshToken() {
+    }
+
+    public RefreshToken(Long id, String token, Instant expiryDate, boolean revoked, User user) {
+        this.id = id;
+        this.token = token;
+        this.expiryDate = expiryDate;
+        this.revoked = revoked;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Instant getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Instant expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public boolean isRevoked() {
+        return revoked;
+    }
+
+    public void setRevoked(boolean revoked) {
+        this.revoked = revoked;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+}
