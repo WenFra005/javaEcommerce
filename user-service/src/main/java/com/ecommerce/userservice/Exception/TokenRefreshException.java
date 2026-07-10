a06dd07ebf8189ab30054eee312a8db4f0d8eeab
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-07-10 11:56:37 -0300

    feat(exception): cria exceção para refresh de token

---
+++ user-service/src/main/java/com/ecommerce/userservice/Exception/TokenRefreshException.java
@@ -0,0 +1,9 @@
+package com.ecommerce.userservice.Exception;
+
+public class TokenRefreshException extends RuntimeException {
+    
+    public TokenRefreshException(String message) {
+        super(message);
+    }
+
+}
