5800d821ff866ed11a71bf0fce595dfd6525346b
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-07-11 15:42:23 -0300

    refactor(RefreshToken): melhora mapeamento JPA com anotações explícitas
    Adiciona importações `FetchType` e `OneToOne`, e atualiza as anotações `@Column` com nomes explícitos (`id`, `token`, `expiry_date`, `revoked`, `user_id`). Altera o relacionamento com `User` para `@OneToOne(fetch = FetchType.LAZY)` para definir explicitamente a estratégia de carregamento.

--- user-service/src/main/java/com/ecommerce/userservice/Model/RefreshToken.java
+++ user-service/src/main/java/com/ecommerce/userservice/Model/RefreshToken.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 
 @Entity(name = "refresh_tokens")
@@ -28,7 +29,7 @@ public class RefreshToken {
     private boolean revoked;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @Column(name="user_id",nullable = false)
+    @JoinColumn(name="user_id",nullable = false)
     private User user;
 
     public RefreshToken() {
