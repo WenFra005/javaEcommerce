dcdfb112478e6dbfb6996d11b14fc955601a954b
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-07-24 14:45:45 -0300

    refator(dto): adiciona validações de campos obrigatórios para CreateAdminRequest
    Implementa validações de campos obrigatórios para os campos 'name', 'email' e 'password' no DTO CreateAdminRequest com mensagens de erro específicas.

--- user-service/src/main/java/com/ecommerce/userservice/dto/CreateAdminRequest.java
+++ user-service/src/main/java/com/ecommerce/userservice/dto/CreateAdminRequest.java
@@ -1,17 +1,21 @@
 package com.ecommerce.userservice.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 
 @Schema(description = "Dados necessários para criação de um usuário administrador.")
 public class CreateAdminRequest {
 
     @Schema(description = "Nome do administrador", example = "Administrador Principal")
+    @NotBlank(message = "Name is required")
     private String name;
 
     @Schema(description = "E-mail do administrador", example = "admin@example.com")
+    @NotBlank(message = "Email is required")
     private String email;
 
     @Schema(description = "Senha do administrador", example = "admin@12345")
+    @NotBlank(message = "Password is required")
     private String password;
 
     public CreateAdminRequest(String name, String email, String password) {
