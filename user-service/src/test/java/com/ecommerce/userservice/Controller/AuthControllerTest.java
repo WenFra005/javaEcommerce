7402be870bfa120253c3092435f9e01780167e2d
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-07-24 13:44:47 -0300

    cleanup(user-service/test): remove import unused 'never' Remove the unnecessary import of `org.mockito.Mockito.never` used by AuthControllerTest.

--- user-service/src/test/java/com/ecommerce/userservice/Controller/AuthControllerTest.java
+++ user-service/src/test/java/com/ecommerce/userservice/Controller/AuthControllerTest.java
@@ -3,7 +3,6 @@ package com.ecommerce.userservice.Controller;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -11,7 +10,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
