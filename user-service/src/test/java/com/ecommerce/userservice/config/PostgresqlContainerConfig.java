abca2bc800b65367d0902eb6d81f9175e3a85bb7
WenFra005 <wendellfrancisco2005@hotmail.com>
2026-08-03 14:14:36 -0300

    test(container): adiciona configuração de container PostgreSQL para testes
    Cria um `PostgresqlContainerConfig` que inicializa um container PostgreSQL 15‑alpine com banco, usuário e senha predefinidos.
    Essa configuração facilita a execução de testes de integração que dependem de um banco de dados, sem precisar de um serviço externo.

---
+++ user-service/src/test/java/com/ecommerce/userservice/config/PostgresqlContainerConfig.java
@@ -0,0 +1,21 @@
+package com.ecommerce.userservice.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class PostgresqlContainerConfig {
+
+    @Bean
+    @ServiceConnection
+    @SuppressWarnings("resource")
+    public PostgreSQLContainer<?> postgreSQLContainer() {
+        return new PostgreSQLContainer<>("postgres:15-alpine")
+                .withDatabaseName("testdb")
+                .withUsername("testuser")
+                .withPassword("testpass");
+    }
+
+}
